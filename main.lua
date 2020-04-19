5af47da5dd66321d641ac0fcb409580322b0dcbf
Szolderits Chiara <c.szolderits@gmail.com>
2020-04-19 17:33:20 +0200

    last changes

--- main.lua
+++ main.lua
@@ -1,27 +1,25 @@
 WIDTH = love.graphics.getWidth()
 HEIGHT = love.graphics.getHeight()
+MAX_SCORE = 6
+STATE = ''
+BALL_SIZE = 5
+
+PADDLE_SPEED = 400
+PADDLE_WIDTH = 15
+PADDLE_HIGHT = 80
 
 function love.load()
-    newGame()
-    ballsize = 5
+    serveBall()
 
-	-- dimensions from paddles
-    paddleWidth = 15
-    paddleHeight = 80
-    paddle1X = 0
-    paddle1Y = HEIGHT / 2 - paddleHeight
-    paddle2X = WIDTH - paddle1X - paddleWidth
-    paddle2Y = HEIGHT/ 2 - paddleHeight
-    paddleSpeed = 400
+    paddleL_x = 0
+    paddleL_y = HEIGHT / 2 - PADDLE_HIGHT
+    paddleR_x = WIDTH - paddleL_x - PADDLE_WIDTH
+    paddleR_y = HEIGHT/ 2 - PADDLE_HIGHT
 
-    maxScore = 6
+    STATE = 'startScreen'
+    gameOverMsg = ''
 
-    currentState = 'startScreen'
-    gameOverMessage = ''
-
-	--love.graphics.setBackgroundColor(255, 240, 245)
-	--love.graphics.setColor(176, 048, 096, 1)
-	font = love.graphics.newFont('font.ttf', 50)
+    font = love.graphics.newFont('font.ttf', 50)
     love.graphics.setFont(font)
     love.window.setTitle('Pong by Chiara Szolderits')
 
@@ -33,34 +31,50 @@ function love.load()
 end
 
 function love.update(dt)
-    if currentState == 'play' then
+    if STATE == 'play' then
         play(dt)
     end
 end
 
 function love.draw()
-	love.graphics.setColor(176, 048, 096)
-	if currentState == 'startScreen' then
-		startScreen()
-	    player1Score = 0
-	    player2Score = 0
-	elseif currentState == 'ready' then
-        ready()
-    elseif currentState == 'play' then
+    red = 139/255
+    green = 0/255
+    blue = 139/255
+	love.graphics.setBackgroundColor(red, green, blue)
+
+    -- startscreen --
+    if STATE == 'startScreen' then
+        love.graphics.print('Welcome to Pong!\nPress Space to Start!\nPress Escape to quit\n\nPlayer with 6 points wins!', 3, 3)
+	    Score_PlayerL = 0
+        Score_PlayerR = 0
+    
+    -- ready --
+	elseif STATE == 'ready' then
+        love.graphics.print('Press space to serve \n\nPlayer with 6 points wins! \n\n       up      down\nleft     w      s\nright  up      down', 3, 3)
+    
+    -- play --   
+    elseif STATE == 'play' then
+        red = 216/255
+        green = 191/255
+        blue = 216/255
+        love.graphics.setColor(red, blue, green)
         drawGraphics()
-    elseif currentState == 'gameOver' then
-        gameOver()
+
+    -- game over --    
+    elseif STATE == 'gameOver' then
+        love.graphics.print(gameOverMsg, 10, 10)
     end
 end
 
+-- move between states --
 function love.keypressed(key)
     if key == 'space' then
-		if currentState == 'startScreen' then
-			currentState = 'ready'
-		elseif currentState == 'ready' then
-            currentState = 'play'
-        elseif currentState == 'gameOver' then
-            currentState = 'startScreen'
+		if STATE == 'startScreen' then
+			STATE = 'ready'
+        elseif STATE == 'ready' then
+            STATE = 'play'
+        elseif STATE == 'gameOver' then
+            STATE = 'startScreen'
         end
     end
     if key == 'escape' then
@@ -68,122 +82,113 @@ function love.keypressed(key)
     end
 end
 
-function newGame()
-    ballX = WIDTH / 2
-    ballY = HEIGHT / 2
+function serveBall()
+    ball_x = WIDTH / 2
+    ball_y = HEIGHT / 2
 	
 	ballSpeed = 650
-	ballDirection = math.rad(math.random(200, 395))
+	ballDirection = math.rad(math.random(100, 395))
 	
 	ballSpeedX = math.cos(ballDirection) * ballSpeed *-1
 	ballSpeedY = math.sin(ballDirection) * ballSpeed
 end
 
+-- play the game --
 function play(dt)
-    ballX = ballX + ballSpeedX * dt
-    ballY = ballY + ballSpeedY * dt
+    ball_x = ball_x + ballSpeedX * dt
+    ball_y = ball_y + ballSpeedY * dt
 
 	-- give the point, when paddle doesnt hit the ball (left & right)
-    if ballX + ballsize < 0 then
-        newGame()
-        player2Score = player2Score + 1
+    if ball_x + BALL_SIZE < 0 then
+        serveBall()
+        Score_PlayerR = Score_PlayerR + 1
         sounds['score']:play()
-        if player2Score == maxScore then
-            currentState = 'gameOver'
-            gameOverMessage = 'Right Player wins!\nPress space to play again'
+        if Score_PlayerR == MAX_SCORE
+    then
+            STATE = 'gameOver'
+            gameOverMsg = 'Right Player wins!\nPress space to play again'
         else
-            currentState = 'ready'
+            STATE = 'ready'
         end
     end
 
-    if ballX > WIDTH then
-        newGame()
-        player1Score = player1Score + 1
+    if ball_x > WIDTH then
+        serveBall()
+        Score_PlayerL = Score_PlayerL + 1
         sounds['score']:play()
-        if player1Score == maxScore then
-            currentState = 'gameOver'
-            gameOverMessage = 'Left Player wins!\nPress space to play again'
+        if Score_PlayerL == MAX_SCORE then
+            STATE = 'gameOver'
+            gameOverMsg = 'Left Player wins!\nPress space to play again'
         else
-            currentState = 'ready'
+            STATE = 'ready'
         end
     end
 	
 	-- check walls up and down
-    if ballY < 0 then
-        ballY = 0
+    if ball_y < 0 then
+        ball_y = 0
         ballSpeedY = -ballSpeedY
         sounds['wall_hit']:play()
     end
 
-    if ballY > HEIGHT then
-        ballY = HEIGHT
+    if ball_y > HEIGHT then
+        ball_y = HEIGHT
         ballSpeedY = -ballSpeedY
         sounds['wall_hit']:play()
     end
 
 	-- move paddle
     if love.keyboard.isDown('w') then
-        paddle1Y = paddle1Y - paddleSpeed * dt
+        paddleL_y = paddleL_y - PADDLE_SPEED * dt
 	elseif love.keyboard.isDown('s') then
-        paddle1Y = paddle1Y + paddleSpeed * dt
+        paddleL_y = paddleL_y + PADDLE_SPEED * dt
 	elseif love.keyboard.isDown('up') then
-        paddle2Y = paddle2Y - paddleSpeed * dt
+        paddleR_y = paddleR_y - PADDLE_SPEED * dt
     elseif love.keyboard.isDown('down') then
-        paddle2Y = paddle2Y + paddleSpeed * dt
+        paddleR_y = paddleR_y + PADDLE_SPEED * dt
     end
 
-    paddle1Y = math.clamp(paddle1Y, 0, love.graphics.getHeight() - paddleHeight)
-    paddle2Y = math.clamp(paddle2Y, 0, love.graphics.getHeight() - paddleHeight)
+    paddleL_y = paddlePos(paddleL_y, 0, HEIGHT - PADDLE_HIGHT)
+    paddleR_y = paddlePos(paddleR_y, 0, WIDTH - PADDLE_HIGHT)
 
     checkCollision()
 end
 
 function drawGraphics()
-    love.graphics.circle('fill', ballX, ballY, ballsize)
+    love.graphics.circle('fill', ball_x, ball_y, BALL_SIZE)
 
-	love.graphics.setColor(199, 21, 133)
-    love.graphics.rectangle('fill', paddle1X, paddle1Y, paddleWidth, paddleHeight)
-    love.graphics.rectangle('fill', paddle2X, paddle2Y, paddleWidth, paddleHeight)
+    love.graphics.rectangle('fill', paddleL_x, paddleL_y, PADDLE_WIDTH, PADDLE_HIGHT)
+    love.graphics.rectangle('fill', paddleR_x, paddleR_y, PADDLE_WIDTH, PADDLE_HIGHT)
 
-    love.graphics.print(player1Score, 75, 10)
-    love.graphics.print(player2Score, love.graphics.getWidth() - 150, 10)
+    love.graphics.print(Score_PlayerL, 75, 10)
+    love.graphics.print(Score_PlayerR, love.graphics.getWidth() - 150, 10)
 end
 
-
-function startScreen()
-	love.graphics.setColor(255,182,193)
-	love.graphics.print('Welcome to Pong!\nPress Space to Start!\nPress Escape to quit\n\nPlayer with 6 points wins!', 3, 3)
-end
-
-function ready()
-    love.graphics.print('Press space to serve \n\nPlayer with 6 points wins! \n\n       up      down\nleft     w      s\nright  up      down', 3, 3)
-end
-
-function gameOver()
-    love.graphics.print(gameOverMessage, 10, 10)
-end
-
-function math.clamp(value, min, max)
-    return math.max(math.min(value, max), min)
+function paddlePos(val, min, max)
+    if val < min then
+        val = min
+    elseif max < val then
+    end
+    return val
 end
 
 function checkCollision()
-    if  ballX < paddle1X + paddleWidth and
-        ballX + ballsize > paddle1X and
-        ballY < paddle1Y + paddleHeight and
-        ballY + ballsize > paddle1Y
+    if  ball_x < paddleL_x + PADDLE_WIDTH and
+        ball_x + BALL_SIZE > paddleL_x and
+        ball_y < paddleL_y + PADDLE_HIGHT and
+        ball_y + BALL_SIZE > paddleL_y
     then
-        ballX = paddle1X + paddleWidth
+        ball_x = paddleL_x + PADDLE_WIDTH
         ballSpeedX = -ballSpeedX
         sounds['paddle_hit']:play()
     end
 
-    if  ballX < paddle2X + paddleWidth and
-        ballX + ballsize > paddle2X and
-        ballY < paddle2Y + paddleHeight and
-        ballY + ballsize > paddle2Y
+    if  ball_x < paddleR_x + PADDLE_WIDTH and
+        ball_x + BALL_SIZE > paddleR_x and
+        ball_y < paddleR_y + PADDLE_HIGHT and
+        ball_y + BALL_SIZE > paddleR_y
     then
-        ballX = paddle2X - ballsize
+        ball_x = paddleR_x - BALL_SIZE
         ballSpeedX = -ballSpeedX
         sounds['paddle_hit']:play()
     end
